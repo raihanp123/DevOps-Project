d67a7ce60ec69fdc0bc28d3835bbd67e0e9ceeaa
raihanp123 <86321052+raihanp123@users.noreply.github.com>
2021-08-31 17:04:59 +0100

    Delete backend directory

--- backend/application/models.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from application import db
-
-class Users(db.Model):
-    userName = db.Column(db.String(50), primary_key=True)
-
-    def as_dict(self):
-        return {"userName":self.userName}
